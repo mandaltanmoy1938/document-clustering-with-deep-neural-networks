2a47671da5051615679b52cdc60b50a99e2b015a
Tanmoy Mandal <mandaltanmoy1938@gmail.com>
2019-12-18 20:32:13 +0100

    chore!: add path

--- code/global_variables.py
+++ code/global_variables.py
@@ -1,7 +1,8 @@
 error_file_count = 0
 
 # src_path = "G:\\UB\\5th Sem\\Individual Project\\Data\\QS-OCR-Large\\"
-src_path = "/media/remote/tmandal/document-clustering-with-deep-neural-networks/data/QS-OCR-Large/"
+# src_path = "/media/remote/tmandal/document-clustering-with-deep-neural-networks/data/QS-OCR-Large/"
+src_path = "/media/tma/Uni/UB/5th Sem/Individual Project/Data/QS-OCR-Large/"
 test_label_file_name = "text_test.txt"
 train_label_file_name = "text_train.txt"
 val_label_file_name = "text_val.txt"
