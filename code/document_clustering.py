d19d15866467a7cfdf6f7ea2022230dadf6a4068
Tanmoy Mandal <mandaltanmoy1938@gmail.com>
2020-03-25 10:11:54 +0100

    fix: time.time_executed; chore: gitignore remove log

--- code/document_clustering.py
+++ code/document_clustering.py
@@ -119,4 +119,4 @@ if __name__ == '__main__':
         main()
     except Exception as ex:
         log.error(ex)
-    time.time_executed(start, "Document clustering")
+    timer.time_executed(start, "Document clustering")
