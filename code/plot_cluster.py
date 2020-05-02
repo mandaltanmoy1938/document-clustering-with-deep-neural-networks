3f1615a44c2c4b0954b08941388aed7018331d57
Tanmoy Mandal <mandaltanmoy1938@gmail.com>
2020-05-02 11:58:22 +0200

    chore: plot doc2vec test ground truth

--- code/plot_cluster.py
+++ code/plot_cluster.py
@@ -30,31 +30,31 @@ def run():
     test_vector_embedded = TSNE(n_components=2).fit_transform(test_vector)
     op.save_object(test_vector_embedded, gv.prj_src_path + "python_objects/test_2d_data_vector")
 
-    test_embedded = loadPickle("test_2d_data_transformed")
-    # test_embedded = loadPickle("test_2d_data_vector")
+    # test_embedded = loadPickle("test_2d_data_transformed")
+    test_embedded = loadPickle("test_2d_data_vector")
     fig_num = 0
     df = pd.DataFrame(test_embedded, columns=["x", "y"])
-    for algo in predicted_label["supervised"]:
-        fig_num += 1
-        predict = loadPickle(algo + "test_data_transformed_predict")
-        df[algo + "prediction"] = [target_names[p] for p in predict]
-        gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
-                        fig_num=fig_num, l_col=3, hue=algo + "prediction")
+    # for algo in predicted_label["supervised"]:
+    #     fig_num += 1
+    #     predict = loadPickle(algo + "test_data_transformed_predict")
+    #     df[algo + "prediction"] = [target_names[p] for p in predict]
+    #     gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #                     fig_num=fig_num, l_col=3, hue=algo + "prediction")
+    #
+    # for algo in predicted_label["unsupervised"]:
+    #     fig_num += 1
+    #     predict = loadPickle(algo + "test_data_transformed_predict")
+    #     df[algo + "prediction"] = predict
+    #     gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #                     fig_num=fig_num, l_col=2, hue=algo + "prediction")
 
-    for algo in predicted_label["unsupervised"]:
-        fig_num += 1
-        predict = loadPickle(algo + "test_data_transformed_predict")
-        df[algo + "prediction"] = predict
-        gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
-                        fig_num=fig_num, l_col=2, hue=algo + "prediction")
-
-        labels = loadPickle("test_labels")
-        labels = [gv.translation[x] for x in labels]
-        df["ground_truth"] = [target_names[l] for l in labels]
-        fig_num += 1
-        gg.plot_cluster(title="Ground truth", data=df, pad=30,
-                        plot_name=gv.prj_src_path + "generated_plots/ground_truth", fig_num=fig_num, l_col=3,
-                        hue="ground_truth")
+    labels = loadPickle("test_labels")
+    labels = [gv.translation[x] for x in labels]
+    df["ground_truth"] = [target_names[l] for l in labels]
+    fig_num += 1
+    gg.plot_cluster(title="Ground truth Doc2Vec", data=df, pad=30,
+                    plot_name=gv.prj_src_path + "generated_plots/ground_truth_doc2vec", fig_num=fig_num, l_col=3,
+                    hue="ground_truth")
 
 
 def main():
