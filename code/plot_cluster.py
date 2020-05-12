a4bfdf07df46b07fd588b921dd2afc251ea61b8d
Tanmoy Mandal <mandaltanmoy1938@gmail.com>
2020-05-12 11:41:59 +0200

    chore: comment out lines to generate only ground truth

--- code/plot_cluster.py
+++ code/plot_cluster.py
@@ -17,48 +17,69 @@ def loadPickle(filename):
 
 
 def run():
-    # predicted_label = {"unsupervised": ["KMeans_"],
-    #                    "supervised": ["LogisticRegression_", "NB_", "SVC_"]}
+    predicted_label = {"unsupervised": ["KMeans_"],
+                       "supervised": ["LogisticRegression_", "NB_", "SVC_"]}
     target_names = [gv.label_name[i] for i in gv.translation_rev]
-    log.debug(target_names)
-    log.debug(len(target_names))
 
-    # dimension reduction
-    # test_transformed = loadPickle("test_data_transformed")
-    # test_transformed_embedded = TSNE(n_components=2).fit_transform(test_transformed)
-    # op.save_object(test_transformed_embedded, gv.prj_src_path + "python_objects/test_2d_data_transformed")
+    # dimension reduction dictvectorizer
+    test_transformed = loadPickle("test_data_transformed")
+    test_transformed_embedded = TSNE(n_components=2).fit_transform(test_transformed)
+    op.save_object(test_transformed_embedded, gv.prj_src_path + "python_objects/test_2d_data_transformed")
 
-    # test_vector = loadPickle("test_vector")
-    # test_vector_embedded = TSNE(n_components=2).fit_transform(test_vector)
-    # op.save_object(test_vector_embedded, gv.prj_src_path + "python_objects/test_2d_data_vector")
+    # dimension reduction doc2vec
+    test_vector = loadPickle("test_vector")
+    test_vector_embedded = TSNE(n_components=2).fit_transform(test_vector)
+    op.save_object(test_vector_embedded, gv.prj_src_path + "python_objects/test_2d_data_vector")
 
-    # test_embedded = loadPickle("test_2d_data_transformed")
-    test_embedded = loadPickle("test_2d_data_vector")
+    test_embedded_dictvectorizer = loadPickle("test_2d_data_transformed")
+    test_embedded_doc2vec = loadPickle("test_2d_data_vector")
     fig_num = 0
-    df = pd.DataFrame(test_embedded, columns=["x", "y"])
+    df_dictvectorizer = pd.DataFrame(test_embedded_dictvectorizer, columns=["x", "y"])
     # for algo in predicted_label["supervised"]:
     #     fig_num += 1
     #     predict = loadPickle(algo + "test_data_transformed_predict")
-    #     df[algo + "prediction"] = [target_names[p] for p in predict]
-    #     gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #     df_dictvectorizer[algo + "prediction"] = [target_names[p] for p in predict]
+    #     gg.plot_cluster(title=algo, data=df_dictvectorizer, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
     #                     fig_num=fig_num, l_col=3, hue=algo + "prediction")
     #
     # for algo in predicted_label["unsupervised"]:
     #     fig_num += 1
     #     predict = loadPickle(algo + "test_data_transformed_predict")
-    #     df[algo + "prediction"] = predict
-    #     gg.plot_cluster(title=algo, data=df, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #     df_dictvectorizer[algo + "prediction"] = predict
+    #     gg.plot_cluster(title=algo, data=df_dictvectorizer, pad=30, plot_name=gv.prj_src_path + "generated_plots/" + algo,
     #                     fig_num=fig_num, l_col=2, hue=algo + "prediction")
 
     labels = loadPickle("test_labels")
     labels = [gv.translation[x] for x in labels]
-    log.debug(len(labels))
-    log.debug(set(labels))
-    log.debug(len(set(labels)))
-    log.debug(df.shape)
-    df["ground_truth"] = [target_names[l] for l in labels]
+    log.debug(df_dictvectorizer.shape)
+    df_dictvectorizer["ground_truth"] = [target_names[l] for l in labels]
     fig_num += 1
-    gg.plot_cluster(title="Ground truth Doc2Vec", data=df, pad=30,
+    gg.plot_cluster(title="Ground truth Dictvectorizer", data=df_dictvectorizer, pad=30,
+                    plot_name=gv.prj_src_path + "generated_plots/ground_truth_dictvectorizer", fig_num=fig_num, l_col=3,
+                    hue="ground_truth")
+
+    df_doc2vec = pd.DataFrame(test_embedded_doc2vec, columns=["x", "y"])
+    # for algo in predicted_label["supervised"]:
+    #     fig_num += 1
+    #     predict = loadPickle(algo + "test_data_transformed_predict")
+    #     df_doc2vec[algo + "prediction"] = [target_names[p] for p in predict]
+    #     gg.plot_cluster(title=algo, data=df_doc2vec, pad=30,
+    #                     plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #                     fig_num=fig_num, l_col=3, hue=algo + "prediction")
+    #
+    # for algo in predicted_label["unsupervised"]:
+    #     fig_num += 1
+    #     predict = loadPickle(algo + "test_data_transformed_predict")
+    #     df_doc2vec[algo + "prediction"] = predict
+    #     gg.plot_cluster(title=algo, data=df_doc2vec, pad=30,
+    #                     plot_name=gv.prj_src_path + "generated_plots/" + algo,
+    #                     fig_num=fig_num, l_col=2, hue=algo + "prediction")
+
+    labels = loadPickle("test_labels")
+    labels = [gv.translation[x] for x in labels]
+    df_doc2vec["ground_truth"] = [target_names[l] for l in labels]
+    fig_num += 1
+    gg.plot_cluster(title="Ground truth Doc2Vec", data=df_doc2vec, pad=30,
                     plot_name=gv.prj_src_path + "generated_plots/ground_truth_doc2vec", fig_num=fig_num, l_col=3,
                     hue="ground_truth")
 
